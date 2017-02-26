7aeec04bcc8dcbf72f666d6ab4072cf5613b38df
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-02-27 00:05:54 +0200

    minor improvements

--- members/views.py
+++ members/views.py
@@ -5,18 +5,16 @@ from .models import Member
 
 # Create your views here.
 def new_member_application(request):
-    print(request.POST['email_addr'])
-
     payload = request.POST
-
-    if Member.objects.filter(email_addr=payload['email_addr']):
-        return HttpResponse("S-pode käytössä.")
+    if not payload:
+        return HttpResponse("Tarviin tiedot.")
+    elif Member.objects.filter(email_addr=payload['email_addr']):
+        return HttpResponse("S-pode jonkun muun (??) käytössä.")
     else:
         new_applicant = Member(
             name=payload['name'],
             place_of_residence=payload['place_of_residence'],
             email_addr=payload['email_addr'])
-
         new_applicant.save()
 
     return HttpResponse("Tallennettu.")
